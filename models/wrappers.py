085637b7afd29c9addc52eff120f4ff2efec1613
Kailas D S <55548603+kailasds@users.noreply.github.com>
2023-02-27 10:04:21 +0530

    change

--- models/wrappers.py
+++ models/wrappers.py
@@ -146,7 +146,7 @@ class StyleGAN2(BaseModel):
             'places': 'https://drive.google.com/uc?export=download&id=1X8-wIH3aYKjgDZt4KMOtQzN1m4AlCVhm',
             'bedrooms': 'https://drive.google.com/uc?export=download&id=1nZTW7mjazs-qPhkmbsOLLA_6qws-eNQu',
             'kitchen': 'https://drive.google.com/uc?export=download&id=15dCpnZ1YLAnETAPB0FGmXwdBclbwMEkZ',
-            'lookbook': 'https://drive.google.com/uc?export=download&id=1-F-RMkbHUv_S_k-_olh43mu5rDUMGYKe'
+            'lookbook': 'https://mega.nz/file/DdVgVKgC#RmK61qWRgoJvt0VmOktXyIkq4Cpc2E0dJMwmSI3vF5s'
         }
 
         url = checkpoints[self.outclass]
